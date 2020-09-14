74a540f9e2854469b559228b4c3c1c8e3b8e5e95
thiyagu <thiyagu@programmer.net>
2020-09-14 22:55:56 +0530

    initial

---
+++ src/app/pages/layout/list/GameDataPojo.ts
@@ -0,0 +1,7 @@
+export class GameDataPojo {
+    id: number;
+    player_name: string;
+    current_score: string;
+    high_score: string;
+
+}
---
+++ src/app/pages/layout/list/list.component.ts
@@ -0,0 +1,93 @@
+// import { Component } from '@angular/core';
+// import { fruits } from './fruits-list';
+// import { current_score } from './fruits-list';
+// import { maximum_score } from './fruits-list';
+//
+//
+//
+// @Component({
+//   selector: 'ngx-list',
+//   templateUrl: 'list.component.html',
+//   styleUrls: ['list.component.scss'],
+// })
+// export class ListComponent
+//
+// {
+//   fruits = fruits;
+//   current_score = current_score;
+//   maximum_score=maximum_score;
+//
+//   users: { name: string, title: string }[] = [
+//     { name: 'Carla Espinosa', title: 'Nurse' },
+//     { name: 'Bob Kelso', title: 'Doctor of Medicine' },
+//     { name: 'Janitor', title: 'Janitor' },
+//     { name: 'Perry Cox', title: 'Doctor of Medicine' },
+//     { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+//   ];
+//
+//
+//
+// }
+import { Component ,OnInit} from '@angular/core';
+import { fruits } from './fruits-list';
+import { Observable } from "rxjs";
+import { current_score } from './fruits-list';
+import { maximum_score } from './fruits-list';
+import {GameServices} from './GameServices'
+import {GameDataPojo} from './GameDataPojo'
+import { HttpClient } from '@angular/common/http';
+
+
+interface SearchResults {
+    total: number;
+    results: Array<object>;
+}
+@Component({
+  selector: 'ngx-list',
+  templateUrl: 'list.component.html',
+  styleUrls: ['list.component.scss'],
+})
+export class ListComponent implements OnInit
+
+{
+
+
+
+  game_data: GameDataPojo;
+  fruits = fruits;
+  current_score = current_score;
+  maximum_score=maximum_score;
+totalAngularPackages;
+  users: { name: string, title: string }[] = [
+    { name: 'Carla Espinosa', title: 'Nurse' },
+    { name: 'Bob Kelso', title: 'Doctor of Medicine' },
+    { name: 'Janitor', title: 'Janitor' },
+    { name: 'Perry Cox', title: 'Doctor of Medicine' },
+    { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+  ];
+
+ constructor(private http: HttpClient) { }
+
+// ngOnInit() {
+//    this.reloadData();
+//    console.log("asdsdsd");
+//  }
+
+ngOnInit() {
+        // Simple GET request with response type <any>
+        this.http.get<SearchResults>('http://localhost:8000/').subscribe(data => {
+    //this.totalAngularPackages = data.total;
+    this.game_data = data.results;
+    console.log(data.results);
+})
+    }
+
+reloadData() {
+    // this.game_data = this.GameServices.getGameDataList();
+    // console.log(this.game_data[0].id);
+    this.http.get<any>('https://api.npms.io/v2/search?q=scope:angular').subscribe(data => {
+        this.totalAngularPackages = data.total;
+    })
+
+  }
+}
